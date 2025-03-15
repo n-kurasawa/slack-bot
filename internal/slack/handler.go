7099a180c9f9dc35159a9cb12f389bf6ba30334e
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2025-03-16 08:48:38 +0900

    画像表示にMsgOptionAttachmentsを使用するように変更

--- internal/slack/handler.go
+++ internal/slack/handler.go
@@ -120,9 +120,14 @@ func (h *Handler) handleMessage(event *slackevents.MessageEvent) error {
 			return fmt.Errorf("画像の取得に失敗: %w", err)
 		}
 
+		attachment := slackapi.Attachment{
+			Title:    fmt.Sprintf("画像 ID: %d", img.ID),
+			ImageURL: img.URL,
+		}
+
 		_, _, err = h.client.PostMessage(
 			event.Channel,
-			slackapi.MsgOptionText(fmt.Sprintf("画像 ID: %d\n%s", img.ID, img.URL), false),
+			slackapi.MsgOptionAttachments(attachment),
 		)
 		if err != nil {
 			return fmt.Errorf("メッセージの送信に失敗: %w", err)
